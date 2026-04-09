76f306a06407ee81fe865590379e4cf685f1e30d
yung0328jm <yuyung8230@gmail.com>
2026-04-09 14:38:37 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        'gold': '#FFD700',
-        'dark-gray': '#2C2C2C',
-        'charcoal': '#1A1A1A',
+        gold: '#FFD700',
+        'dark-gray': '#252018',
+        charcoal: '#14110e',
+        cn: {
+          ink: '#0f0c0a',
+          lacquer: '#1c1512',
+          panel: '#231c18',
+          vermilion: '#b83226',
+          gold: '#d4af37',
+          'gold-dim': '#9a7b2c',
+          parchment: '#f0e6d4',
+          jade: '#2f6f5e',
+          mist: '#8a8078',
+        },
+      },
+      fontFamily: {
+        serif: ['"Noto Serif TC"', 'STSong', 'SimSun', 'serif'],
       },
     },
   },
